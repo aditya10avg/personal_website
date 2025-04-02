94bf11dab5ae8bb2d76b14bdd3edac4ecb65ca13
aditya10avg <adityavg1005@gmail.com>
2025-04-02 18:04:06 +0530

    Added many quotes

--- src/pages/QuotesPage.tsx
+++ src/pages/QuotesPage.tsx
@@ -1,16 +1,59 @@
 const quotes = [
   {
-    text: "Impatience with action and patience with result.",
-    author: "Aditya"
+    text: "Be impatient with action and patient with result.",
   },
   {
-    text: "Humans are dynamic, there are times a person might be introvert, there are times a person might be an extrovert, there will be a context in which you will be pessimistic and there will be a context in which you will be optimistic.",
-    author: "Aditya"
+    text: "Humans are dynamic, there are times a person might be introvert, there are times a person might be an extrovert, there will be a context in which you will be pessimistic and there will be a context in which you will be optimistic. Your traits are dynamic and changes with conditions you are in.",
+    },
+  {
+    text: "When you fail there are infinite-1 more opportunities i.e. Infinite more opportunities.",
+    
   },
   {
-    text: "When you fail there are infinite - 1 more opportunities i.e. Infinite more opportunities.",
-    author: "Aditya"
-  }
+    text: "Your real resume is the catalogue of all your sufferings.", 
+  },
+  {
+    text: "Any freedom that desires for more freedom is not a freedom.",
+  },
+  {
+    text: "The only way to do great work is to love what you do.",
+  },
+  {
+    text:"The solution to any problem is not the solution you think it is but the problem itself. It is somewhere deeply rooted in the problem.Go backwards to solve that problem",
+  },
+  {
+    text: "The only way to do great work is to love what you do.",
+  },
+  {
+    text: "The only way to do great work is to love what you do.",
+  },
+  {
+    text: "Life is not the journey as you think it is most often, it has a destination. If there is no destination there would be no journey. There would be no compulsion to start your journey at all without a destination.",
+  },
+  {
+    text: "Hard work doesn't mean efficient work.",
+  },
+  {
+    text: "We are naturally drawn to things when we immersed into them. That's Surrounding matters.",
+  },
+  {
+    text: "'Should' and 'Shouldn't' to do anything are pure poison.",
+  },
+  {
+    text: "No technique of the mind will free you from the mind.",
+  },
+  {   
+    text: "There is no prescription to do great things. But there is for normal work. You can't do great things by just following the prescription/roadmap.",
+  },
+  {
+    text: "Innovation is the child of freedom and parent of prosperity.",
+  },
+  {
+    text: "The goal of education is to teach a person to think and reason, not to simply load the memory with thoughts of other men.",
+  },
+  {
+    text: "Education is not the learning of facts, but the training of the mind to think.",
+  },
 ];
 
 const QuotesPage = () => {
